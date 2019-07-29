38c4dd2dc61f7bf3d24947fc18667026c99f67f3
Owen Williams <williams.owen@gmail.com>
2019-07-29 19:56:17 +0100

    suggested changes from pull request + moved processing,org example to processing folder as arduino editor was converting it to an ino

--- examples/RollPitchYaw/RollPitchYaw.ino
+++ examples/RollPitchYaw/RollPitchYaw.ino
@@ -1,17 +1,17 @@
 /**
- * What is this file?  
- * This is an arduino example that calculates accurate roll,pitch,yaw from raw gyro/accelerometer data
- * It has a calibration stage which removes most of the gyro drift and a complementary filter 
- * that combines gyro and accelerometer angles to produce roll/pitch angles that don't drift (like the gyro angle) and aren't noisy 
- * (like the accel angles). As there is no magnetic compass on the nano iot 33, it's not possible to 'complement' the yaw 
- * - hence yaw will drift and is 'relative'.
- */
+   What is this file?
+   This is an arduino example that calculates accurate roll,pitch,yaw from raw gyro/accelerometer data
+   It has a calibration stage which removes most of the gyro drift and a complementary filter
+   that combines gyro and accelerometer angles to produce roll/pitch angles that don't drift (like the gyro angle) and aren't noisy
+   (like the accel angles). As there is no magnetic compass on the nano iot 33, it's not possible to 'complement' the yaw
+   - hence yaw will drift and is 'relative'.
+*/
 #include <Arduino_LSM6DS3.h>
 #include <Wire.h>
 
 float accelX,            accelY,             accelZ,            // units m/s/s i.e. accelZ if often 9.8 (gravity)
-      gyroX,             gyroY,              gyroZ,             // units dps (degrees per second) 
-      gyroDriftX,        gyroDriftY,         gyroDriftZ,        // units dps 
+      gyroX,             gyroY,              gyroZ,             // units dps (degrees per second)
+      gyroDriftX,        gyroDriftY,         gyroDriftZ,        // units dps
       gyroRoll,          gyroPitch,          gyroYaw,           // units degrees (expect major drift)
       gyroCorrectedRoll, gyroCorrectedPitch, gyroCorrectedYaw,  // units degrees (expect minor drift)
       accRoll,           accPitch,           accYaw,            // units degrees (roll and pitch noisy, yaw not possible)
@@ -26,7 +26,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   // this sketch will wait until something connects to serial!
-  // this could be 'arduino monitor', 'arduino plotter' or 'processing.org P3D client (see pde file)' 
+  // this could be 'serial monitor', 'serial plotter' or 'processing.org P3D client' (see ./processing/RollPitchYaw3d.pde file)
   while (!Serial);
 
   if (!IMU.begin()) {
@@ -35,17 +35,17 @@ void setup() {
   }
 
   calibrateIMU(250, 250);
-  
+
   lastTime = micros();
 
 }
 
-/* 
-  the gyro's x,y,z values drift by a steady amount. if we measure this when arduino is still 
+/*
+  the gyro's x,y,z values drift by a steady amount. if we measure this when arduino is still
   we can correct the drift when doing real measurements later
 */
 void calibrateIMU(int delayMillis, int calibrationMillis) {
-  
+
   int calibrationCount = 0;
 
   delay(delayMillis); // to avoid shakes after pressing reset button
@@ -58,7 +58,7 @@ void calibrateIMU(int delayMillis, int calibrationMillis) {
       sumX += gyroX;
       sumY += gyroY;
       sumZ += gyroZ;
-  
+
       calibrationCount++;
     }
   }
@@ -73,12 +73,12 @@ void calibrateIMU(int delayMillis, int calibrationMillis) {
 
 }
 
-/** 
- * Read accel and gyro data.   
- * returns true if value is 'new' and false if IMU is returning old cached data
- */
+/**
+   Read accel and gyro data.
+   returns true if value is 'new' and false if IMU is returning old cached data
+*/
 bool readIMU() {
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() ){
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() ) {
     IMU.readAcceleration(accelX, accelY, accelZ);
     IMU.readGyroscope(gyroX, gyroY, gyroZ);
     return true;
@@ -88,7 +88,7 @@ bool readIMU() {
 
 // the loop function runs over and over again forever
 void loop() {
-  
+
   if (readIMU()) {
     long currentTime = micros();
     lastInterval = currentTime - lastTime; // expecting this to be ~104Hz +- 4%
@@ -98,37 +98,37 @@ void loop() {
     printCalculations();
 
   }
-  
+
 }
 
 /**
- * I'm expecting, over time, the Arduino_LSM6DS3.h will add functions to do most of this,
- * but as of 1.0.0 this was missing.  
- */
+   I'm expecting, over time, the Arduino_LSM6DS3.h will add functions to do most of this,
+   but as of 1.0.0 this was missing.
+*/
 void doCalculations() {
-  accRoll = atan2(accelY, accelZ) * 180/M_PI;
-  accPitch = atan2(-accelX, sqrt(accelY*accelY + accelZ*accelZ)) * 180/M_PI;
+  accRoll = atan2(accelY, accelZ) * 180 / M_PI;
+  accPitch = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180 / M_PI;
 
   float lastFrequency = 1000000 / lastInterval;
-  gyroRoll = gyroRoll + (gyroX/lastFrequency);
-  gyroPitch = gyroPitch + (gyroY/lastFrequency);
-  gyroYaw = gyroYaw + (gyroZ/lastFrequency);
+  gyroRoll = gyroRoll + (gyroX / lastFrequency);
+  gyroPitch = gyroPitch + (gyroY / lastFrequency);
+  gyroYaw = gyroYaw + (gyroZ / lastFrequency);
 
-  gyroCorrectedRoll = gyroCorrectedRoll + ((gyroX - gyroDriftX)/lastFrequency);
-  gyroCorrectedPitch = gyroCorrectedPitch + ((gyroY - gyroDriftY)/lastFrequency);
-  gyroCorrectedYaw = gyroCorrectedYaw + ((gyroZ - gyroDriftZ)/lastFrequency);
+  gyroCorrectedRoll = gyroCorrectedRoll + ((gyroX - gyroDriftX) / lastFrequency);
+  gyroCorrectedPitch = gyroCorrectedPitch + ((gyroY - gyroDriftY) / lastFrequency);
+  gyroCorrectedYaw = gyroCorrectedYaw + ((gyroZ - gyroDriftZ) / lastFrequency);
 
-  complementaryRoll = complementaryRoll + ((gyroX - gyroDriftX)/lastFrequency);
-  complementaryPitch = complementaryPitch + ((gyroY - gyroDriftY)/lastFrequency);
-  complementaryYaw = complementaryYaw + ((gyroZ - gyroDriftZ)/lastFrequency);
+  complementaryRoll = complementaryRoll + ((gyroX - gyroDriftX) / lastFrequency);
+  complementaryPitch = complementaryPitch + ((gyroY - gyroDriftY) / lastFrequency);
+  complementaryYaw = complementaryYaw + ((gyroZ - gyroDriftZ) / lastFrequency);
 
-  complementaryRoll = 0.98*complementaryRoll + 0.02*accRoll;
-  complementaryPitch = 0.98*complementaryPitch + 0.02*accPitch;
+  complementaryRoll = 0.98 * complementaryRoll + 0.02 * accRoll;
+  complementaryPitch = 0.98 * complementaryPitch + 0.02 * accPitch;
 }
 
 /**
- * This comma separated format is best 'viewed' using 'arduino plotter' or processing.org client (see pde example)
- */
+   This comma separated format is best 'viewed' using 'serial plotter' or processing.org client (see ./processing/RollPitchYaw3d.pde example)
+*/
 void printCalculations() {
   Serial.print(gyroRoll);
   Serial.print(',');
@@ -141,7 +141,7 @@ void printCalculations() {
   Serial.print(gyroCorrectedPitch);
   Serial.print(',');
   Serial.print(gyroCorrectedYaw);
-  Serial.print(',');      
+  Serial.print(',');
   Serial.print(accRoll);
   Serial.print(',');
   Serial.print(accPitch);
