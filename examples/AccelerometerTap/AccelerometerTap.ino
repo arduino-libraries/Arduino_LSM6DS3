1618d6b8609f0d7a73ec3ca4eb9672b47f91948a
tkhabia <tanmay.khabia@students.iiit.ac.in>
2020-03-17 09:59:55 +0530

    resolve #3

--- examples/AccelerometerTap/AccelerometerTap.ino
+++ examples/AccelerometerTap/AccelerometerTap.ino
@@ -1,4 +1,4 @@
-/* 
+/*
   Arduino LSM6DS3 - Accelerometer Tap
 
   this code is to detect tap
@@ -18,24 +18,24 @@ void setup() {
     delay(3000); // wait for 3 sec and check if it can be initialized again
   }
 }
-float tapThreshold  = 0.05; //0.05 g acceleration in some direction is considered as tap. it can be change for the required sensitivity.
+float tapThreshold  = 0.05; //0.05 g acceleration in some direction is considered as tap. it can be changed for the required sensitivity.
 
-int down = 3; // signifying the direction of which is facing downward 1 for x axis ; 2 for y axis ; 3 for z axis;
+int down = 3; // signifying the direction of which is facing downward 1 for x axis; 2 for y axis; 3 for z axis;
 
 void loop() {
   float x, y, z;
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
-    
+
     if ((x > tapThreshold || x < -tapThreshold) && down != 1)  {
       Serial.println("Tap detected across X-axis");
     }
-    
+
     if ((y > tapThreshold || y < -tapThreshold) && down != 2)   {
       Serial.println("Tap detected across Y-axis");
     }
-    
-    if ((z > tapThreshold || z < -tapThreshold)&& down != 3)   {
+
+    if ((z > tapThreshold || z < -tapThreshold) && down != 3)   {
       Serial.println("Tap detected across Z-axis");
     }
   }
