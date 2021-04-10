37e37457e69467ca2a51aaf6d2e0b4de149053e8
per1234 <accounts@perglass.com>
2021-04-10 15:53:53 -0700

    Correct typos in comments and documentation

--- src/LSM6DS3.h
+++ src/LSM6DS3.h
@@ -32,14 +32,14 @@ class LSM6DS3Class {
     void end();
 
     // Accelerometer
-    virtual int readAcceleration(float& x, float& y, float& z); // Results are in G (earth gravity).
+    virtual int readAcceleration(float& x, float& y, float& z); // Results are in g (earth gravity).
     virtual float accelerationSampleRate(); // Sampling rate of the sensor.
-    virtual int accelerationAvailable(); // Check for available data from accerometer
+    virtual int accelerationAvailable(); // Check for available data from accelerometer
 
     // Gyroscope
     virtual int readGyroscope(float& x, float& y, float& z); // Results are in degrees/second.
     virtual float gyroscopeSampleRate(); // Sampling rate of the sensor.
-    virtual int gyroscopeAvailable(); // Check for available data from gyroscopeAvailable
+    virtual int gyroscopeAvailable(); // Check for available data from gyroscope
 
 
   private:
